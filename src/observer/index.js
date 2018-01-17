e22755b562a3d7876cf6fbd4cb34c4b1b577f02a
enrico.cicconi <enrico.cicconi@peertribe.com>
2018-01-17 18:32:02 +0100

    server startup function for test driven functions

--- src/observer/index.js
+++ src/observer/index.js
@@ -1,11 +1,12 @@
 'use strict'
 
-module.exports = function (Paratii) {
+module.exports = function (Paratii, registry) {
   var module = {}
   let paratii = new Paratii({
     provider: 'http://localhost:8545'
   })
-
+  // just for testing
+  // paratii.setRegistry(registry)
   module.videoObserver = require('./video')(paratii)
   module.userObserver = require('./user')(paratii)
 
--- src/server.js
+++ src/server.js
@@ -4,19 +4,30 @@ const express = require('express')
 const compression = require('compression')
 const paratiilib = require('paratii-lib')
 
-// Overlooking Blockchain obSERVER
-const observer = require('./observer')(paratiilib.Paratii)
-observer.videoObserver.init()
-observer.userObserver.init()
-
 const api = require('./api/v1')
+let observer = null
+
 require('./db')
 
 const app = express()
-app.use(compression())
-app.use(express.json())
+startServer('address')
 
-app.use('/api/v1', api)
+function startServer (registry) {
+  // Overlooking Blockchain obSERVER
+  observer = require('./observer')(paratiilib.Paratii, registry)
+  observer.videoObserver.init()
+  observer.userObserver.init()
+  if (registry) {
+    app.use(compression())
+    app.use(express.json())
 
-app.listen(3000)
-console.log('listening at 3000')
+    app.use('/api/v1', api)
+
+    app.listen(3000)
+    console.log('listening at 3000')
+  } else {
+    throw Error('Registry address needed!')
+  }
+}
+
+module.exports.startServer = startServer
