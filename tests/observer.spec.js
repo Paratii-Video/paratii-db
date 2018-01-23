371790cc4f6f24b59c2aa731a597aef35e8ce341
enrico.cicconi <enrico.cicconi@peertribe.com>
2018-01-23 13:14:59 +0100

    upgrade web3

--- tests/observer.spec.js
+++ tests/observer.spec.js
@@ -71,4 +71,43 @@ describe('# Paratii-db Observer', function (done) {
       return new Promise(resolve => setTimeout(resolve, ms))
     }
   })
+  it('subscription to Remove Video events should work as expected', function (done) {
+    let creator = accounts[1].publicKey
+    let price = 3 * 10 ** 18
+    let ipfsHash = 'xyz'
+    let ipfsData = 'zzz'
+    let number = Math.random() // 0.9394456857981651
+    var videoId = number.toString(36).substr(2, 9)
+    // not so elegant, it would be better to wait for server, observer, api ecc.
+    sleep(2000).then(function () {
+      paratii.eth.vids.create({
+        id: videoId,
+        price: price,
+        owner: creator,
+        ipfsHash: ipfsHash,
+        ipfsData: ipfsData
+      })
+
+      waitUntil()
+      .interval(500)
+      .times(5)
+      .condition(function (cb) {
+        let condition = false
+        Video.findOne({_id: videoId}).exec().then(function (video) {
+          if (video) {
+            condition = (video._id === videoId)
+            cb(condition)
+          }
+        })
+      })
+      .done(function (result) {
+        assert.equal(true, result)
+        done()
+      })
+    })
+
+    function sleep (ms) {
+      return new Promise(resolve => setTimeout(resolve, ms))
+    }
+  })
 })
