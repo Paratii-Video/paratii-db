4a9e788d48d62475bccaddd2768cea5c5815395c
enrico.cicconi <enrico.cicconi@peertribe.com>
2018-01-23 12:40:14 +0100

    working creation event and mongo update

--- src/observer/video.js
+++ src/observer/video.js
@@ -7,18 +7,31 @@ const Video = Models.video
 module.exports = function (paratii) {
   var module = {}
 
-  module.init = function () {
+  module.init = async function () {
     // events hook
-    console.log('inizializing all video events')
+
     paratii.eth.web3.setProvider('ws://localhost:8546')
-    paratii.eth.events.addListener('CreateVideo', function (log) {
-      console.log(log)
+
+    await paratii.eth.events.addListener('CreateVideo', function (log) {
       Video.upsert(parser.video(log), (err, vid) => {
         if (err) {
           throw err
         }
       })
     })
+    console.log('inizialized all video events')
+
+    // let removeVideo = await paratii.eth.events.addListener('RemoveVideo', function (log) {
+    //   console.log(log)
+    //   const videoId = log.returnValues.videoId
+    //   Video.deleteMany({_id: videoId}, (err, vid) => {
+    //     if (err) {
+    //       throw err
+    //     }
+    //   })
+    // })
+
+    // console.log(removeVideo)
   }
 
   return module
--- tests/observer.spec.js
+++ tests/observer.spec.js
@@ -7,12 +7,13 @@ const paratiilib = require('paratii-lib')
 const accounts = require('./data/accounts')
 const assert = chai.assert
 const Video = require('../src/models').video
-// const assert = chai.assert
+const waitUntil = require('wait-until')
 // const expect = chai.expect
 chai.use(dirtyChai)
 
-describe('# Paratii-db Observer', function () {
+describe('# Paratii-db Observer', function (done) {
   let paratii
+
   before(async () => {
     paratii = await new paratiilib.Paratii({
       provider: 'http://localhost:8545/rpc/',
@@ -21,12 +22,14 @@ describe('# Paratii-db Observer', function () {
     })
     paratii.eth.web3.setProvider('ws://localhost:8546')
     const contract = await paratii.eth.deployContracts()
-    console.log(contract.Registry.options.address)
-    require('../src/server').start(contract.Registry.options.address)
+    const server = require('../src/server')
+
+    server.start(contract.Registry.options.address)
   })
 
-  it('paratii lib okness', async function () {
+  it('paratii lib okness', async function (done) {
     assert.isOk(paratii)
+    done()
   })
 
   it('subscription to Create Video events should work as expected', function (done) {
@@ -36,24 +39,36 @@ describe('# Paratii-db Observer', function () {
     let ipfsData = 'zzz'
     let number = Math.random() // 0.9394456857981651
     var videoId = number.toString(36).substr(2, 9)
-
-    setTimeout(() => {
+    // not so elegant, it would be better to wait for server, observer, api ecc.
+    sleep(2000).then(function () {
       paratii.eth.vids.create({
         id: videoId,
         price: price,
         owner: creator,
         ipfsHash: ipfsHash,
         ipfsData: ipfsData
-      }).then(function (res) {
-        Video.findOne({_id: videoId}, (err, video) => {
-          if (err) {
-            throw err
-          } else {
-            assert.equal(video._id, videoId)
-            done()
+      })
+
+      waitUntil()
+      .interval(500)
+      .times(5)
+      .condition(function (cb) {
+        let condition = false
+        Video.findOne({_id: videoId}).exec().then(function (video) {
+          if (video) {
+            condition = (video._id === videoId)
+            cb(condition)
           }
         })
       })
-    }, 100)
+      .done(function (result) {
+        assert.equal(true, result)
+        done()
+      })
+    })
+
+    function sleep (ms) {
+      return new Promise(resolve => setTimeout(resolve, ms))
+    }
   })
 })
