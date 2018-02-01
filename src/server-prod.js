ae7876ef2704debdd9a8b633e4da845c2bdef976
enrico.cicconi <enrico.cicconi@peertribe.com>
2018-02-01 19:23:58 +0100

    server prod start temporary

---
+++ src/server-prod.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const express = require('express')
+const compression = require('compression')
+const paratiilib = require('paratii-lib')
+
+const api = require('./api/v1')
+let observer = null
+
+require('./db')
+
+const app = express()
+
+start()
+
+async function start () {
+  // Overlooking Blockchain obSERVER
+
+  // observer = require('./observer')(paratiilib.Paratii)
+  // observer.videoObserver.init()
+  // observer.userObserver.init()
+  // observer.transactionObserver.init()
+
+  app.use(compression())
+  app.use(express.json())
+
+  app.use('/api/v1', api)
+
+  app.listen(3000)
+  console.log('listening at 3000')
+}
+
+module.exports.start = start
