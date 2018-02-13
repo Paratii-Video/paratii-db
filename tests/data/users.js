811313c478685d991cda5e28d536ba470f67508e
enrico.cicconi <enrico.cicconi@peertribe.com>
2018-02-13 15:25:25 +0100

    fix fictures

--- tests/data/users.js
+++ tests/data/users.js
@@ -19,7 +19,7 @@ module.exports = [{
   'name': 'Thanks Forallthe Gino'
 },
 {
-  '_id': '0xa99dBd162ad5E1601E8d8B20703e5A3bA5c00Be4',
+  '_id': '0xa99dBd162ad5E1601E8d8B20703e5A3bA5c00Be7',
   'email': 'emailtarget@humbert.ru',
   'ipfsHash': 'some-hash',
   'name': 'Humbert Humbert Gino'
