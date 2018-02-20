467d3feed5683b180acfaf5d67438797ff852855
enrico.cicconi <enrico.cicconi@peertribe.com>
2018-02-20 18:25:20 -0300

    adding cors settings

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -4,6 +4,29 @@ const router = express.Router()
 const videoAPI = require('./video')
 const userAPI = require('./user')
 const transactionAPI = require('./transaction')
+
+const cors = require('cors')
+const whitelist = require('./cors.json').whitelisted
+let corsOptions = {}
+
+if (process.env.NODE_ENV === 'production') {
+  corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
+} else if (process.env.NODE_ENV === 'development') {
+  corsOptions = {
+    origin: function (origin, callback) {
+      callback(null, true)
+    }
+  }
+}
+
 // const searchAPI = require('./search')
 
 router.get('/', (req, res, next) => {
@@ -11,8 +34,8 @@ router.get('/', (req, res, next) => {
 })
 
 // router.use('/search', searchAPI)
-router.use('/videos', videoAPI)
-router.use('/users', userAPI)
-router.use('/transactions', transactionAPI)
+router.use('/videos', cors(corsOptions), videoAPI)
+router.use('/users', cors(corsOptions), userAPI)
+router.use('/transactions', cors(corsOptions), transactionAPI)
 
 module.exports = router
