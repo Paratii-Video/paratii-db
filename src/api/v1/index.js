939d1aa6b5e8f380d7b34cd597d43a38c270e6c8
enrico.cicconi <enrico.cicconi@peertribe.com>
2018-02-26 20:02:59 -0300

    adding cors origin in logs for debugging

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'production') {
       if (whitelist.indexOf(origin) !== -1) {
         callback(null, true)
       } else {
-        callback(new Error('Not allowed by CORS'))
+        callback(new Error(origin + 'Not allowed by CORS'))
       }
     }
   }
