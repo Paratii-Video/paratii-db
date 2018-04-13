5c9bb6baccd877251181609acb2004b2a4284313
Geckoslair <enrico@piramid.studio>
2018-04-13 13:14:55 +0200

    format filter on videos search

--- src/api/v1/video.js
+++ src/api/v1/video.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 const Models = require('../../models')
 const Video = Models.video
+const Json2csvParser = require('json2csv').Parser
 
 router.get('/:id/related', (req, res, next) => {
   // get related videoss
@@ -38,7 +39,20 @@ router.get('/', (req, res, next) => {
       return res.send(err).statusCode(500)
     }
     // TODO: add query params, total, start, limit and results
-    return res.json(result)
+
+    if (req.query.format === 'csv') {
+      const fields = ['_id', 'title', 'description', 'price', 'duration', 'author', 'createBlockNumber', 'filesize']
+      const json2csvParser = new Json2csvParser({fields})
+      const csv = json2csvParser.parse(result.results)
+      if (req.query.download) {
+        res.setHeader('Content-disposition', 'attachment; filename=data.csv')
+        res.set('Content-Type', 'text/csv')
+      }
+
+      res.status(200).send(csv)
+    } else {
+      return res.json(result)
+    }
   })
 })
 
